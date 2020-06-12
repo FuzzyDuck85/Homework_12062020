074f533a9dedbc614872efdaa8af928de40d77e0
Donald Cameron <drc85@live.co.uk>
2020-06-12 15:32:29 +0100

    second commit CritterWatch returns html built

---
+++ js/app.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const newAnimalObservationForm = document.querySelector('#new-animal-observation-form');
+    newAnimalObservationForm.addEventListener('submit', handleNewAnimalObservationForm);
+});
